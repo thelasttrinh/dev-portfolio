73352bd9dee52f4f74da6d3db4a3479ba29a6b45
thelasttrinh <aarontrinh57@yahoo.com>
2023-08-07 11:50:36 -0500

    Adjusted flex for header and added hover to headers

--- src/Containers/Header.jsx
+++ src/Containers/Header.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import '../Styling/App.scss'
 const Header = () => {
   return (
-    <div class="flex items-center">
-        <div class="text-xl font-semibold">Home</div>
-        <div class="text-xl font-semibold ml-auto">
-    <div>About Me</div>
-    <div>Projects</div>
-    <div>Contact</div>
-    <div>Night Mode</div>
+    <div class="header-links">
+        <div class="header-links-hover">Home</div>
+        <div class="header-links-right">
+          <div class="header-links-hover">About Me</div>
+          <div class="header-links-hover">Projects</div>
+          <div class="header-links-hover">Contact</div>
+          <div class="header-links-hover">Night Mode</div>
       </div>
     </div>
   )
