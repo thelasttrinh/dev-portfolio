b3a4e84a8360b55daa31f979a6e18652f648767d
thelasttrinh <aarontrinh57@yahoo.com>
2023-08-23 11:54:43 -0500

    Added icons, adjusted intro

--- src/Containers/Header.jsx
+++ src/Containers/Header.jsx
@@ -5,10 +5,16 @@ import PDF from "../assets/Aaron_Trinh_Resume_5.1.pdf";
 const Header = () => {
   return (
     <div class="header-links">
-      <Link to="/dev-portfolio" class="header-links-hover">Home</Link>
+      
+      <Link to="/dev-portfolio" class="header-links-hover"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 20 20">
+        <path fill="currentColor" d="m16 8.5l1.53 1.53l-1.06 1.06L10 
+        4.62l-6.47 6.47l-1.06-1.06L10 2.5l4 4v-2h2v4zm-6-2.46l6 
+        5.99V18H4v-5.97zM12 17v-5H8v5h4z"></path></svg>
+        </Link>
         <div class="header-links-right">
         <a href={PDF} without rel="noopener noreferrer" target="_blank">
           <button class="header-links-hover resume" label="Resume">
+          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M12 15.575q-.2 0-.375-.062T11.3 15.3l-3.6-3.6q-.3-.3-.288-.7t.288-.7q.3-.3.713-.312t.712.287L11 12.15V5q0-.425.288-.713T12 4q.425 0 .713.288T13 5v7.15l1.875-1.875q.3-.3.713-.288t.712.313q.275.3.288.7t-.288.7l-3.6 3.6q-.15.15-.325.213t-.375.062ZM6 20q-.825 0-1.413-.588T4 18v-2q0-.425.288-.713T5 15q.425 0 .713.288T6 16v2h12v-2q0-.425.288-.713T19 15q.425 0 .713.288T20 16v2q0 .825-.588 1.413T18 20H6Z"></path></svg>
             Resume
           </button>
         </a>
