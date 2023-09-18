1fa2ae145cd9635e81da255d4589ce52a352a315
thelasttrinh <aarontrinh57@yahoo.com>
2023-09-18 15:51:06 -0500

    Made side menu align with top and right side without gaps

--- src/Containers/Header.jsx
+++ src/Containers/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
   }
   return (
     <div class="header-links">
-      <Link to="/dev-portfolio" class="header-links-hover">
+      <Link to="/dev-portfolio" class="header-hover">
         <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 20 20">
         <path fill="currentColor" d="m16 8.5l1.53 1.53l-1.06 1.06L10 
         4.62l-6.47 6.47l-1.06-1.06L10 2.5l4 4v-2h2v4zm-6-2.46l6 
@@ -35,17 +35,20 @@ const Header = () => {
         <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M12 15.575q-.2 0-.375-.062T11.3 15.3l-3.6-3.6q-.3-.3-.288-.7t.288-.7q.3-.3.713-.312t.712.287L11 12.15V5q0-.425.288-.713T12 4q.425 0 .713.288T13 5v7.15l1.875-1.875q.3-.3.713-.288t.712.313q.275.3.288.7t-.288.7l-3.6 3.6q-.15.15-.325.213t-.375.062ZM6 20q-.825 0-1.413-.588T4 18v-2q0-.425.288-.713T5 15q.425 0 .713.288T6 16v2h12v-2q0-.425.288-.713T19 15q.425 0 .713.288T20 16v2q0 .825-.588 1.413T18 20H6Z"></path></svg>
           Resume</span>
         </a>
-        <Link to="#about-me" class="header-links-hover">About Me</Link>
-        <div class="header-links-hover">Projects</div>
-        <div class="header-links-hover">Contact</div>
+        <Link to="#about-me" class="header-hover">About Me</Link>
+        <div class="header-hover">Projects</div>
+        <div class="header-hover">Contact</div>
       </div>
       )}
 
-      {!matches && (<svg class="header-links-right header-links-hover" xmlns="http://www.w3.org/2000/svg" width="32" height="32" 
+      <div class="header-hover header-hamburger" onClick={toggleSideMenu}>
+      {!matches && (<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" 
       viewBox="0 0 16 16"><path fill="none" stroke="currentColor" stroke-linecap="round" 
-      stroke-linejoin="round" stroke-width="1.5" d="M2.75 12.25h10.5m-10.5-4h10.5m-10.5-4h10.5" onClick={toggleSideMenu}/></svg>)} 
+      stroke-linejoin="round" stroke-width="1.5" d="M2.75 12.25h10.5m-10.5-4h10.5m-10.5-4h10.5"/></svg>)} 
 
-      {showSideMenu && !matches &&<SideMenu toggleSideMenu = {toggleSideMenu}/>}
+      {showSideMenu && !matches && <SideMenu toggleSideMenu = {toggleSideMenu}/>}
+      </div>
+      
     </div>
   )
 }
--- src/Containers/SideMenu.jsx
+++ src/Containers/SideMenu.jsx
@@ -4,8 +4,8 @@ import '../Styling/_sidemenu.scss'
 const SideMenu = (props) => {
     const {toggleSideMenu} = props;
   return (
-    <div class="sidemenu">SideMenu
-    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 16 16"><path fill="none" stroke="currentColor" stroke-linecap="round" 
+    <div class="sidemenu">
+    <svg class="sidemenu-content" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 16 16"><path fill="none" stroke="currentColor" stroke-linecap="round" 
     stroke-linejoin="round" stroke-width="1.5" 
     d="m11.25 4.75l-6.5 6.5m0-6.5l6.5 6.5" onClick={toggleSideMenu}/></svg>
     </div>
