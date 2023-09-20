1f80cffa3bdb88ed3a948fea0a6b715f3cff91dc
thelasttrinh <aarontrinh57@yahoo.com>
2023-09-20 16:45:14 -0500

    Added links to side menu via mapping

--- src/Containers/Header.jsx
+++ src/Containers/Header.jsx
@@ -9,14 +9,54 @@ const Header = () => {
     window.matchMedia("(min-width: 768px)").matches
   )
   useEffect(() => {
-    window
-    .matchMedia("(min-width: 768px)")
-    .addEventListener('change', e => setMatches( e.matches ));
+    const mediaQuery = window.matchMedia("(min-width: 768px)");
+
+    const handleMediaQueryChange = (e) => {
+      setMatches(e.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      // Clean up the event listener when the component unmounts
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
   }, []);
 
   const toggleSideMenu = () => {
     setShowSideMenu(!showSideMenu);
   }
+
+  const headerLinks = [
+    {
+      to: "/dev-portfolio",
+      text: (<a href={PDF} without rel="noopener noreferrer" target="_blank" class="relative inline-block px-4 py-2 font-medium group">
+      <span class="absolute inset-0 w-full h-full bg-primary rounded"></span>
+      <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform group-hover:-translate-x-1 group-hover:-translate-y-1 bg-navy border-2 border-primary group-hover:bg-navy rounded"></span>
+      <span class="flex items-center gap-2 relative text-lightest_slate group-hover:text-lightest_slate rounded transition duration-200 ease-out transform group-hover:-translate-x-1 group-hover:-translate-y-1">
+      <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M12 15.575q-.2 0-.375-.062T11.3 15.3l-3.6-3.6q-.3-.3-.288-.7t.288-.7q.3-.3.713-.312t.712.287L11 12.15V5q0-.425.288-.713T12 4q.425 0 .713.288T13 5v7.15l1.875-1.875q.3-.3.713-.288t.712.313q.275.3.288.7t-.288.7l-3.6 3.6q-.15.15-.325.213t-.375.062ZM6 20q-.825 0-1.413-.588T4 18v-2q0-.425.288-.713T5 15q.425 0 .713.288T6 16v2h12v-2q0-.425.288-.713T19 15q.425 0 .713.288T20 16v2q0 .825-.588 1.413T18 20H6Z"></path></svg>
+        Resume</span>
+      </a>
+      ),
+      classes: "header-hover",
+    },
+    {
+      to: "#about-me",
+      text: "About Me",
+      classes: "header-hover",
+    },
+    {
+      to: "#projects", // Replace with the actual link or route
+      text: "Projects",
+      classes: "header-hover",
+    },
+    {
+      to: "#contact", // Replace with the actual link or route
+      text: "Contact",
+      classes: "header-hover",
+    },
+  ];
+
   return (
     <div class="header-links">
       <Link to="/dev-portfolio" class="header-hover">
@@ -39,7 +79,7 @@ const Header = () => {
         <Link to="#about-me" class="header-hover">About Me</Link>
         <div class="header-hover">Projects</div>
         <div class="header-hover">Contact</div>
-          </div>
+      </div>
         ) : (
           <div className="header-hover header-hamburger" onClick={toggleSideMenu}>
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 16 16">
@@ -55,7 +95,7 @@ const Header = () => {
           </div>
         )}
       </div>
-      {showSideMenu && !matches && <SideMenu toggleSideMenu={toggleSideMenu} />}
+      {showSideMenu && !matches && <SideMenu toggleSideMenu={toggleSideMenu} headerLinks={headerLinks}/>}
       
     </div>
   )
--- src/Containers/SideMenu.jsx
+++ src/Containers/SideMenu.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import '../Styling/_sidemenu.scss'
 
 const SideMenu = (props) => {
-    const {toggleSideMenu} = props;
+    const {toggleSideMenu, headerLinks} = props;
   return (
     <div class="sidemenu">
-    <svg class="sidemenu-content" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 16 16"><path fill="none" stroke="currentColor" stroke-linecap="round" 
+    <svg class="sidemenu-exit" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 16 16"><path fill="none" stroke="currentColor" stroke-linecap="round" 
     stroke-linejoin="round" stroke-width="1.5" 
     d="m11.25 4.75l-6.5 6.5m0-6.5l6.5 6.5" onClick={toggleSideMenu}/></svg>
+    <div class="sidemenu-links">
+    {headerLinks.map((link, index) => (
+      <Link to={link.to} key={index} className={link.classes}>
+        {link.text}
+      </Link>))
+      }
+    </div>
+
     </div>
   )
 }
